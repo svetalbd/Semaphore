9f9a7a169f48382b891a63315cb1020ff7e71583
mykhailenko.s@ukr.net <mykhailenko.s@ukr.net>
2016-06-12 15:33:14 +0300

    При помощи методов wait(), notify(), notifyAll() реализовать семафор

---
+++ Runner.java
@@ -0,0 +1,55 @@
+package com.goit.semaphores;
+
+import javafx.concurrent.Worker;
+
+import java.util.Random;
+
+/**
+ * Created by Сергей on 12.06.2016.
+ */
+public class Runner {
+
+    private SemaphoreImpl semaphore;
+    Random next = new Random();
+    int permits = 5;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Runner().test();
+        System.out.println("\n The second test is beginning \n");
+        new Runner().testForConstant();
+    }
+
+    public void test() throws InterruptedException {
+        semaphore = new SemaphoreImpl(10);
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Number of permits: " + semaphore.getAvailablePermits());
+                    semaphore.acquire(permits);
+                    System.out.println("Thread " + Thread.currentThread().getName() + " started");
+                    semaphore.release(permits);
+                }
+            }).start();
+        }
+    }
+
+    public void testForConstant() throws InterruptedException {
+        semaphore = new SemaphoreImpl(2, 8);
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Number of permits: " + semaphore.getAvailablePermits());
+                    semaphore.acquire(next.nextInt(8));
+                    System.out.println("Thread " + Thread.currentThread().getName() + " started");
+                    semaphore.release(next.nextInt(8));
+                }
+            }).start();
+        }
+    }
+
+
+
+}
+
---
+++ SemaphoreImpl.java
@@ -0,0 +1,78 @@
+package com.goit.semaphores;
+
+/**
+ * Created by Сергей on 10.06.2016.
+ */
+public class SemaphoreImpl implements Semaphore {
+    private volatile int freePermits = 0;
+    private final Object lock = new Object();
+    private final int MAX_FLOW_NUMBER;
+
+    public SemaphoreImpl(int MAX_FLOW_NUMBER, int freePermits) {
+        this.MAX_FLOW_NUMBER = MAX_FLOW_NUMBER;
+        this.freePermits = freePermits;
+    }
+
+    public SemaphoreImpl(int freePermits) {
+        this.MAX_FLOW_NUMBER = freePermits;
+        this.freePermits = freePermits;
+    }
+
+
+    @Override
+    public void acquire() {
+        synchronized (lock) {
+            if (freePermits > 0)
+                freePermits--;
+            else {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void acquire(int permits) {
+        if (freePermits < permits && freePermits > 0 && permits > 0) {
+            System.out.println("ERROR: free permits = [" + freePermits + "]");
+            System.out.println("ERROR: permits = [" + permits + "]");
+        } else {
+            synchronized (lock) {
+                freePermits -= permits;
+            }
+        }
+    }
+
+    @Override
+    public void release() {
+        synchronized (lock) {
+            if (freePermits < MAX_FLOW_NUMBER) {
+                freePermits++;
+                lock.notify();
+            }
+        }
+
+    }
+
+    @Override
+    public void release(int permits) {
+        synchronized (lock) {
+            if (permits < 0) {
+                System.out.println("ERROR: free permits = [" + freePermits + "]");
+                System.out.println("ERROR: permits = [" + permits + "]");
+            }
+            if (freePermits + permits >=MAX_FLOW_NUMBER) {
+                freePermits += permits;
+                lock.notify();
+            }
+        }
+    }
+
+    @Override
+    public int getAvailablePermits() {
+        return freePermits;
+    }
+}
